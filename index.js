779d75dceecb28031b144d0d742cd981a2d4403f
Jeffrey T Gosselin <jgoz.email@gmail.com>
2019-01-28 18:08:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,6 @@ function createFareMultiplier(number) {
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
-
+function fetchSpecifiedDrivers(drivers) {
+  
+}
