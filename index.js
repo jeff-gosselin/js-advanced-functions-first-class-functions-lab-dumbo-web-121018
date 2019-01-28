94deb08bf8a51e5b9f3ac74397bed0e93d30f2ed
Jeffrey T Gosselin <jgoz.email@gmail.com>
2019-01-28 17:32:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const returnLastTwoDrivers = function (drivers) {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-function createFareMultiplier() {
+function createFareMultiplier(number) {
   
 }
